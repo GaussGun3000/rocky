ffe8186b5afb03e0e6efed2ca96caaffb389b013
Jason Beverage <jasonbeverage@gmail.com>
2026-05-14 11:10:16 -0400

    Reverted changes to build.bat and rocky_shell.bat

--- rocky_shell.bat
+++ rocky_shell.bat
@@ -1,8 +1,7 @@
-cd ..
 set PATH=%CD%\install\bin;%PATH%
 set VCPKG_DIR=%CD%\build\vcpkg_installed
-set PATH=%VCPKG_DIR%\x64-windows\bin;%PATH%
-set PATH=%VCPKG_DIR%\x64-windows\plugins;%PATH%
-set PATH=%VCPKG_DIR%\x64-windows\tools\proj;%PATH%
-set GDAL_DATA=%VCPKG_DIR%\x64-windows\share\gdal
-set PROJ_DATA=%VCPKG_DIR%\x64-windows\share\proj
+set PATH=%VCPKG_DIR%\x64-windows-release\bin;%PATH%
+set PATH=%VCPKG_DIR%\x64-windows-release\plugins;%PATH%
+set PATH=%VCPKG_DIR%\x64-windows-release\tools\proj;%PATH%
+set GDAL_DATA=%VCPKG_DIR%\x64-windows-release\share\gdal
+set PROJ_DATA=%VCPKG_DIR%\x64-windows-release\share\proj
